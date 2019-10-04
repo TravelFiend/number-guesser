6b33aacbc36e84e79350c5e9005d625a2775899e
Joseph Tatum <josephtatum@gmail.com>
2019-10-04 09:52:29 -0700

    created super strech goal in compare.test.js

--- compare.js
+++ compare.js
@@ -1,6 +1,6 @@
 const compareNumbers = (guess, correctNum) => {
     if (guess < 1 || guess > 20 || typeof Number(guess.value) !== 'number'){
-        return false;
+        throw 'error';
     } else if (guess > correctNum){
         return 1;
     } else if (guess < correctNum){
